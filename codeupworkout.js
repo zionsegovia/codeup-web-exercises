45d8c259a50cbc0dbfc4814d2c74421ccf017790
zion segovia <zionsegovia@gmail.com>
2023-02-09 14:54:36 -0600

    adding data-operators exercise

---
+++ codeupworkout.js
@@ -0,0 +1,51 @@
+
+
+//let sample = ("Hello Codeup");
+//sample += "Students";
+//let length = sample.length;
+//let result = sample.toUpperCase();
+//let result2 = sample.replace("Students!", "Class");
+//console.log(sample.indexOf('c'));
+//console.log(sample.indexOf('C'));
+//console.log(sample.substring(6,12));
+
+//let TLM = 3
+//let BB = 5
+//let H = 1
+//let total = ((TLM + BB + H)*3
+
+//let googleTotal = 400
+//let googlePay = (googleTotal * 6);
+//let amazonTotal = 380
+//let amazonPay = (amazonTotal * 4);
+//let facebookTotal = 350
+//let facebookPay = (facebookTotal * 10)
+
+//let classnotfull = true
+//let classschedule = true
+//let enrolled = classnotfull && classschedule;
+
+//let haspremium = true
+//let cartsize = 4
+//let validoffer = true
+//let HaveDiscount = validoffer && (cartsize > 2 || haspremium);
+
+
+let username = "codeup";
+let password = "notastrongpassword"
+if password.length > 5 = true
+else password.length < 5 = false
+
+if username.length < 20 = true
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ js/javascript-intro.js
@@ -0,0 +1,17 @@
+//let var1 = true
+//let var2 = 356321.01
+//let var3 = "Hello Codeup"
+//let var4 = "356321.01"
+//let var5 = "2 + 2"
+//let var6 = 'single quote string'
+//let var7 = "jason's been drinking"
+//let var8 = `It wouldn't matter if I "used" single or double`
+
+//let var9 = 123
+//let var10= false
+//let var11= 'true'
+//let var12 = true
+
+
+//et var13; //undefined
+
