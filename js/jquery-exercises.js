15a979b3090397bca80125115b2bf4b32823d38c
zion segovia <zionsegovia@gmail.com>
2023-03-17 12:23:52 -0500

    JQuery Workout

---
+++ js/jquery-exercises.js
@@ -0,0 +1,18 @@
+//$("#listStuffId")
+//let listStuffIdText = $("#listStuffId").text();
+//alert(listStuffIdText);
+
+//$(".codeup").css({
+ //   'border': '1px solid red',
+//});
+
+$("li").css({
+   'font-size': '20px'
+});
+$("p, li, h1").css({
+    'color': 'red'
+});
+
+$("h1")
+let headerStuffJQuery = $("h1").text();
+alert(headerStuffJQuery);
